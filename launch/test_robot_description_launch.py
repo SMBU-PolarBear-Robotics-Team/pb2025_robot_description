ed1f0358c2920f9d90160b6ea9a27b3ae9604df5
LihanChen2004 <757003373@qq.com>
2025-01-15 16:57:43 +0800

    🎈 perf: Refactor launch file to use OpaqueFunction and declare launch arguments

--- launch/test_robot_description_launch.py
+++ launch/test_robot_description_launch.py
@@ -1,35 +1,24 @@
 import os
 
 from ament_index_python.packages import get_package_share_directory
-from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration
+from launch import LaunchContext, LaunchDescription
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.substitutions import LaunchConfiguration, TextSubstitution
 from launch_ros.actions import Node
-from sdformat_tools.urdf_generator import UrdfGenerator
 from xmacro.xmacro4sdf import XMLMacro4sdf
 
+from sdformat_tools.urdf_generator import UrdfGenerator
 
-def generate_launch_description():
-    # Get the launch directory
-    pkg_pb2025_robot_description_dir = get_package_share_directory(
-        "pb2025_robot_description"
-    )
 
-    xmacro_description = os.path.join(
-        pkg_pb2025_robot_description_dir,
-        "resource",
-        "xmacro",
-        "pb2025_sentry_robot.sdf.xmacro",
-    )
-    rviz_config_file = os.path.join(
-        pkg_pb2025_robot_description_dir,
-        "rviz",
-        "visualize_robot.rviz",
-    )
+def launch_setup(context: LaunchContext) -> list:
+    """
+    NOTE: Using OpaqueFunction in order to get the context in string format...
+    But it is too hacky and not recommended.
+    """
 
-    use_sim_time = LaunchConfiguration("use_sim_time", default="False")
-
+    # Load the robot xmacro file from the launch configuration
     xmacro = XMLMacro4sdf()
-    xmacro.set_xml_file(xmacro_description)
+    xmacro.set_xml_file(context.launch_configurations["robot_xmacro_file"])
 
     # Generate SDF from xmacro
     xmacro.generate()
@@ -47,7 +36,7 @@ def generate_launch_description():
         output="screen",
         parameters=[
             {
-                "use_sim_time": use_sim_time,
+                "use_sim_time": LaunchConfiguration("use_sim_time"),
             }
         ],
     )
@@ -59,7 +48,7 @@ def generate_launch_description():
         output="screen",
         parameters=[
             {
-                "use_sim_time": use_sim_time,
+                "use_sim_time": LaunchConfiguration("use_sim_time"),
                 "robot_description": robot_urdf_xml,
             }
         ],
@@ -68,16 +57,60 @@ def generate_launch_description():
     start_rviz_node = Node(
         package="rviz2",
         executable="rviz2",
-        arguments=["-d", rviz_config_file],
+        arguments=["-d", LaunchConfiguration("rviz_config_file")],
         output="screen",
+    )
+
+    return [
+        start_joint_state_publisher_node,
+        start_robot_state_publisher_node,
+        start_rviz_node,
+    ]
+
+
+def generate_launch_description():
+    # Get the launch directory
+    bringup_dir = get_package_share_directory("pb2025_robot_description")
+
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        "use_sim_time",
+        default_value="False",
+        description="Use simulation (Gazebo) clock if true",
+    )
+
+    declare_robot_xmacro_name_cmd = DeclareLaunchArgument(
+        "robot_xmacro_name",
+        default_value="pb2025_sentry_robot",
+        description="The name of the robot xmacro to be used",
+    )
+
+    declare_robot_xmacro_file_cmd = DeclareLaunchArgument(
+        "robot_xmacro_file",
+        default_value=[
+            # Use TextSubstitution to concatenate strings
+            TextSubstitution(text=os.path.join(bringup_dir, "resource", "xmacro", "")),
+            LaunchConfiguration("robot_xmacro_name"),
+            TextSubstitution(text=".sdf.xmacro"),
+        ],
+        description="The file path of the robot xmacro to be used",
+    )
+
+    declare_rviz_config_file_cmd = DeclareLaunchArgument(
+        "rviz_config_file",
+        default_value=os.path.join(bringup_dir, "rviz", "visualize_robot.rviz"),
+        description="Full path to the RViz config file to use",
     )
 
     # Create the launch description and populate
     ld = LaunchDescription()
 
-    # Add the actions to launch all nodes
-    ld.add_action(start_joint_state_publisher_node)
-    ld.add_action(start_robot_state_publisher_node)
-    ld.add_action(start_rviz_node)
+    # Declare the launch options
+    ld.add_action(declare_use_sim_time_cmd)
+    ld.add_action(declare_robot_xmacro_name_cmd)
+    ld.add_action(declare_robot_xmacro_file_cmd)
+    ld.add_action(declare_rviz_config_file_cmd)
+
+    # Add the actions to launch all of the nodes
+    ld.add_action(OpaqueFunction(function=launch_setup))
 
     return ld
