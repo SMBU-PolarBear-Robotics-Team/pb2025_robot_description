e40b0b6866c0fabc37e5da578912b7b1325abd03
LihanChen2004 <757003373@qq.com>
2025-03-03 16:44:37 +0800

    🎈 perf: Rewrite launch file. Add params.yaml

--- launch/robot_description_launch.py
+++ launch/robot_description_launch.py
@@ -2,10 +2,17 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchContext, LaunchDescription
-from launch.actions import DeclareLaunchArgument, OpaqueFunction, SetEnvironmentVariable
+from launch.actions import (
+    DeclareLaunchArgument,
+    GroupAction,
+    OpaqueFunction,
+    SetEnvironmentVariable,
+)
 from launch.conditions import IfCondition
 from launch.substitutions import LaunchConfiguration, TextSubstitution
 from launch_ros.actions import Node
+from launch_ros.descriptions import ParameterFile
+from nav2_common.launch import RewrittenYaml
 from sdformat_tools.urdf_generator import UrdfGenerator
 from xmacro.xmacro4sdf import XMLMacro4sdf
 
@@ -16,21 +23,13 @@ def launch_setup(context: LaunchContext) -> list:
     But it is too hacky and not recommended.
     """
 
+    namespace = LaunchConfiguration("namespace")
     use_sim_time = LaunchConfiguration("use_sim_time")
-    source_list = LaunchConfiguration("source_list")
-    offset_timestamp = LaunchConfiguration("offset_timestamp")
+    params_file = LaunchConfiguration("params_file")
     rviz_config_file = LaunchConfiguration("rviz_config_file")
     use_rviz = LaunchConfiguration("use_rviz")
     use_respawn = LaunchConfiguration("use_respawn")
     log_level = LaunchConfiguration("log_level")
-
-    # Map fully qualified names to relative ones so the node's namespace can be prepended.
-    # In case of the transforms (tf), currently, there doesn't seem to be a better alternative
-    # https://github.com/ros/geometry2/issues/32
-    # https://github.com/ros/robot_state_publisher/pull/30
-    # TODO(orduno) Substitute with `PushNodeRemapping`
-    #              https://github.com/ros2/launch_ros/issues/56
-    remappings = [("/tf", "tf"), ("/tf_static", "tf_static")]
 
     # Load the robot xmacro file from the launch configuration
     xmacro = XMLMacro4sdf()
@@ -45,69 +44,75 @@ def launch_setup(context: LaunchContext) -> list:
     urdf_generator.parse_from_sdf_string(robot_xml)
     robot_urdf_xml = urdf_generator.to_string()
 
+    # Create our own temporary YAML files that include substitutions
+    param_substitutions = {"use_sim_time": use_sim_time}
+
+    configured_params = ParameterFile(
+        RewrittenYaml(
+            source_file=params_file,
+            root_key=namespace,
+            param_rewrites=param_substitutions,
+            convert_types=True,
+        ),
+        allow_substs=True,
+    )
+
     stdout_linebuf_envvar = SetEnvironmentVariable(
         "RCUTILS_LOGGING_BUFFERED_STREAM", "1"
     )
 
     colorized_output_envvar = SetEnvironmentVariable("RCUTILS_COLORIZED_OUTPUT", "1")
 
-    start_joint_state_publisher_node = Node(
-        package="joint_state_publisher",
-        executable="joint_state_publisher",
-        name="joint_state_publisher",
-        output="screen",
-        respawn=use_respawn,
-        respawn_delay=2.0,
-        parameters=[
-            {
-                "use_sim_time": use_sim_time,
-                "rate": 200,
-                "source_list": source_list,
-                "offset_timestamp": offset_timestamp,
-            }
-        ],
-        arguments=["--ros-args", "--log-level", log_level],
-        remappings=remappings,
-    )
-
-    start_robot_state_publisher_node = Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        output="screen",
-        respawn=use_respawn,
-        respawn_delay=2.0,
-        parameters=[
-            {
-                "use_sim_time": use_sim_time,
-                "publish_frequency": 200.0,
-                "robot_description": robot_urdf_xml,
-            }
-        ],
-        arguments=["--ros-args", "--log-level", log_level],
-        remappings=remappings,
-    )
-
-    start_rviz_node = Node(
-        condition=IfCondition(use_rviz),
-        package="rviz2",
-        executable="rviz2",
-        arguments=["-d", rviz_config_file],
-        output="screen",
-        remappings=remappings,
+    bringup_cmd_group = GroupAction(
+        [
+            Node(
+                package="joint_state_publisher",
+                executable="joint_state_publisher",
+                name="joint_state_publisher",
+                output="screen",
+                respawn=use_respawn,
+                respawn_delay=2.0,
+                parameters=[configured_params],
+                arguments=["--ros-args", "--log-level", log_level],
+            ),
+            Node(
+                package="robot_state_publisher",
+                executable="robot_state_publisher",
+                output="screen",
+                respawn=use_respawn,
+                respawn_delay=2.0,
+                parameters=[
+                    configured_params,
+                    {"robot_description": robot_urdf_xml},
+                ],
+                arguments=["--ros-args", "--log-level", log_level],
+            ),
+            Node(
+                condition=IfCondition(use_rviz),
+                package="rviz2",
+                executable="rviz2",
+                arguments=["-d", rviz_config_file],
+                output="screen",
+            ),
+        ]
     )
 
     return [
         stdout_linebuf_envvar,
         colorized_output_envvar,
-        start_joint_state_publisher_node,
-        start_robot_state_publisher_node,
-        start_rviz_node,
+        bringup_cmd_group,
     ]
 
 
 def generate_launch_description():
     # Get the launch directory
     bringup_dir = get_package_share_directory("pb2025_robot_description")
+
+    declare_namespace_cmd = DeclareLaunchArgument(
+        "namespace",
+        default_value="",
+        description="Top-level namespace",
+    )
 
     declare_use_sim_time_cmd = DeclareLaunchArgument(
         "use_sim_time",
@@ -132,16 +137,10 @@ def generate_launch_description():
         description="The file path of the robot xmacro to be used",
     )
 
-    declare_source_list_cmd = DeclareLaunchArgument(
-        "source_list",
-        default_value="['serial/gimbal_joint_state']",
-        description="Array of topic names for subscriptions to sensor_msgs/msg/JointStates. Defaults to ['serial/gimbal_joint_state']",
-    )
-
-    declare_offset_timestamp_cmd = DeclareLaunchArgument(
-        "offset_timestamp",
-        default_value="0.0",
-        description="Offset timestamp for joint state publisher",
+    declare_params_file_cmd = DeclareLaunchArgument(
+        "params_file",
+        default_value=os.path.join(bringup_dir, "params", "robot_description.yaml"),
+        description="Full path to the ROS2 parameters file to use for all launched nodes",
     )
 
     declare_rviz_config_file_cmd = DeclareLaunchArgument(
@@ -168,11 +167,11 @@ def generate_launch_description():
     ld = LaunchDescription()
 
     # Declare the launch options
+    ld.add_action(declare_namespace_cmd)
     ld.add_action(declare_use_sim_time_cmd)
     ld.add_action(declare_robot_name_cmd)
     ld.add_action(declare_robot_xmacro_file_cmd)
-    ld.add_action(declare_source_list_cmd)
-    ld.add_action(declare_offset_timestamp_cmd)
+    ld.add_action(declare_params_file_cmd)
     ld.add_action(declare_rviz_config_file_cmd)
     ld.add_action(declare_use_rviz_cmd)
     ld.add_action(declare_use_respawn_cmd)
